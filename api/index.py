6665d49fd5cf57c72b13a2cc0c623e1b84e58ceb
Maithy Le <maithyle265@yahoo.com>
2023-02-05 03:20:15 -0800

    finished get request

--- api/index.py
+++ api/index.py
@@ -15,20 +15,17 @@ class handler(BaseHTTPRequestHandler):
         json_data = cockroachdb.convertJSONdata(json.loads(response.read()))
 
         parsed_url = urlparse(webscraper.BASE_URL + self.path)
-        print("hello")
         print(parsed_url)
         communities = parse_qs(parsed_url.query)['communities'][0]
         communities = communities.split('_')
-        print(communities)
         priceMin = int(parse_qs(parsed_url.query)['priceMin'][0])
         priceMax = int(parse_qs(parsed_url.query)['priceMax'][0])
         bed = int(parse_qs(parsed_url.query)['bed'][0])
-        print(bed)
         bath = float(parse_qs(parsed_url.query)['bath'][0])
-        print(bath)
 
-        #my_dictionary = {"apartments": cockroachdb.filter([(community_list), (priceMin, priceMax), (bed, bath)])}
+        # my_dictionary = {"apartments": cockroachdb.filter([(community_list), (priceMin, priceMax), (bed, bath)])}
         my_dictionary = {"apartments": cockroachdb.filterJSONdata(json_data, communities, priceMin, priceMax, bed, bath)}
+        #my_dictionary = {"apartments": cockroachdb.filterJSONdata(json_data, ["",""], 1000, 1400, 1, 1.5)}
         json_object = json.dumps(my_dictionary, indent=4).encode('utf-8')
         self.send_header('Content-type','text/plain')
         self.end_headers()
